e527f5af19225aec299cbeb68b9ca1519e8ffae8
Mike Potvin <mike@redbitdev.com>
2021-04-16 10:22:56 -0400

    initial commit

---
+++ src/LocalSettingsJsonExtractor/Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text;
+
+namespace LocalSettingsJsonExtractor
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Go to your azure function and open its app settings.
+            // Hit Advanced Edit and copy paste all the contents into "data.json"
+            dynamic data = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText(@"PATH TO data.json"));
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var d in data)
+            {
+                sb.AppendLine($"\"{d.name}\": \"{d.value}\",");
+            }
+
+            // Use included local.settings.json file 
+
+            var localSettings = File.ReadAllText(@"PATH TO local.settings.json");
+
+            localSettings = localSettings.Replace("{REPLACEME}", sb.ToString());
+
+            File.WriteAllText("local.settings.json", localSettings);
+        }
+    }
+}
